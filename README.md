4c98812bfa360634e7b0114903702a0933154bb4
Mariusz Majcher <Majchermx@gmail.com>
2023-02-12 10:49:34 +0000

    done all the work, can be changed in the future

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
-# RANDOM TEXT GENERATOR
+# THE GAME OF CONSEQUENCES
 >
 >
 ## Course from Codecademy called "Mixed messages"
 
-This little program will be generating random text using 3 arrays with specific purpose.
-1. A line of text with 2 places for words to be replaced randomly
-2. An adjective that will be in front of a noun.
-3. An adverb that will be placed after a verb.
+~This little program will be generating random text using 3 arrays with specific purpose.~
+~1. A line of text with 2 places for words to be replaced randomly~
+~2. An adjective that will be in front of a noun.~
+~3. An adverb that will be placed after a verb.~
+
+## Scrap that... This program will have some more variables to make it more interesting
+
+1. A text that will be at the beggining of the short story (setting the time)
+2. Then a person object
+3. Adverb
+4. Another person object
+5. Adverb of time
+6. Averb of manner
+7. Verb
+8. Noun
+9. And finally the very end
+
+## Hope it will put some smiles on peoples faces! :)
 
 There will be a button that will be used to run the program, after pressing it the Message will be displayed.
 
-**Might try to leave the point 2 and 3 for the user to type himself as an option.
+
 
--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-console.log("It works!")
+function random_choice(array) {
+    return  Math.floor(Math.random()*array.length)
+    } 
+
+function text(array) {
+    return array[random_choice(array)]
+}
+
+function upper(text) {
+    return text[0].toUpperCase() + text.slice(1)
+}
+
+const text_start = ["When i was little ", "Once upon a time", "This beautifull evening", 
+                    "On friday evening", "Later the same day", "First thing in the morning",
+                    "When the dinosaurs lived", "When people used led as slug repelent",
+                    "In the last year of that most important century"]
+
+const text_link = ["I", "my dog", "my sister", "she", "my kitten", "that piece of block",
+                     "the puzzle", "a wriggly worm", "this little light", "a giant tree", "my Mommy",
+                    "my Daddy"]
+
+const text_adverb = ["spilled milk", "screamed very loudly", "tripped over a rock", 
+                    "found a purpose", "saw a big red tent", "swam in the river", 
+                    "flew in the sky"]
+
+const adverbs_of_time = ["after that" , "then", "moment later", "afterwards", 
+                     "few minutes after that", "ocasionally"]
+
+const adverbs_of_manner = ["quickly", "kindly", "patiently", "generously", "beautifully", 
+                           "fast"]
+        
+const verb = ["ran", "jumped", "swam", "spied", "climbed", "found"]
+
+const noun = ["a mountain", "a pond", "a river", "a house", "a road"]
+
+const the_end = ["they lived happily ever after", "after that things went right",
+                 "the end", "they never looked back", "till now they laugh about it",
+                 "no one knows about this", "after that, they went back home"]
+
+
+
+const generated_text = () => { 
+        return text(text_start) + " " + text(text_link) + " " + text(text_adverb) + ". " + upper(text(text_link)) + " " + text(adverbs_of_time) + " " + text(adverbs_of_manner) +
+               " " + text(verb) + " " + text(noun) + ". " + upper(text(the_end))}
+
+
+const addText = () => {
+    // const replace = document.getElementsById("main") 
+    // replace.firstChild.nodeValue = "dupa"
+    
+    document.getElementById("text_area").innerHTML = generated_text()
+}   
+
+const button = document.getElementById("button")
+
+
+button.addEventListener("click", () => {addText()}) 
